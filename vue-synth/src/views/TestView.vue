7845680f737d8a075917a2d41736298e0ab01db6
Guillermo Vicente González <guillermovg@usal.es>
2024-04-29 10:54:02 +0200

    configured scss

---
+++ vue-synth/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="testLayout">
+        <h1 class="testLayout__heading">Test view</h1>
+        <h2 class="clase">Esto es otra prueba</h2>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .testLayout{
+        width: 100vw;
+        height: 100vh;
+        background-color: red;
+
+        &__heading{
+            text-align: center;
+            color:white;
+        }
+    }
+</style>
