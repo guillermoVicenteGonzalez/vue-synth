e5e9a9a3156f48c8fb4a9cb7993493db4c1656e7
Guillermo Vicente González <guillermovg@usal.es>
2024-05-07 13:44:54 +0200

    refactored main layout

--- vue-synth/src/Layouts/MainLayout.vue
+++ vue-synth/src/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mainLayout">
+  <!-- <div class="mainLayout">
     <header class="mainLayout__header">
       <slot name="header"> </slot>
     </header>
@@ -15,47 +15,77 @@
     <div class="mainLayout__footer">
       <slot name="footer"> </slot>
     </div>
+  </div> -->
+  <div class="mainLayout">
+    <header class="mainLayout__header">
+      <slot name="header"> </slot>
+    </header>
+
+    <div class="mainLayout__body">
+      <div class="mainLayout__components">
+        <slot name="components"> </slot>
+      </div>
+
+      <div class="mainLayout__mainDisplay">
+        <slot name="display"> </slot>
+      </div>
+    </div>
+
+    <div class="mainLayout__piano">
+      <slot name="piano"> </slot>
+    </div>
+
+    <div class="mainLayout__footer">
+      <slot name="footer"></slot>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+$header-h: 5vh;
+$body-h: 60vh;
+$footer-h: 5vh;
+$piano-h: 30vh;
+
 .mainLayout {
+  box-sizing: border-box;
+  background-color: #222222;
   width: 100vw;
   height: 100vh;
-  //   background-color: red;
-  position: relative;
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
 
   &__header {
-    display: block;
-    height: 4rem;
-    width: 100%;
     background-color: black;
+    color: white;
+    height: $header-h;
     text-align: center;
-    color: #fff;
-  }
-
-  &__footer {
-    height: 4rem;
-    text-align: center;
-    background-color: blue;
-    color: #fff;
-  }
-
-  &__side {
-    display: inline-block;
-    // background-color: yellow;
-    min-width: 10rem;
-    width: 40%;
-    // height: 100%;
   }
 
   &__body {
-    display: inline-block;
+    display: grid;
+    grid-template-columns: 2fr 3fr;
+    height: $body-h;
+  }
 
-    width: 60%;
-    // background-color: green;
-    padding: 2rem;
-    border: solid 1px black;
+  &__components {
+    background-color: white;
+    overflow: hidden;
+  }
+
+  &__mainDisplay {
+    background-color: grey;
+    overflow: hidden;
+  }
+
+  &__piano {
+    height: $piano-h;
+  }
+
+  &__footer {
+    height: $footer-h;
+    background-color: blueviolet;
   }
 }
 </style>
