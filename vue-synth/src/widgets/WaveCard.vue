4e2d6ddca745533994c62504be4b850cefe14b4f
Guillermo Vicente González <GVicenteG@axpe.com>
2024-09-03 13:48:54 +0200

    pause wave

--- vue-synth/src/widgets/WaveCard.vue
+++ vue-synth/src/widgets/WaveCard.vue
@@ -1,8 +1,8 @@
 <template>
   <AudioModuleCard>
-    <div :class="applyDisabled">
+    <div :class="disabledStyles">
       <div class="waveCard__actions-slot">
-        <ToggleButton v-model="disabled"></ToggleButton>
+        <ToggleButton v-model="disabled" @click="disableWave"></ToggleButton>
       </div>
 
       <div class="waveCard__left-slot">
@@ -28,7 +28,7 @@
 
       <div class="waveCard__center-slot">
         <div class="waveCard__wave-slot">
-          <WaveCanvas :wave="wave"></WaveCanvas>
+          <WaveCanvas :wave="wave" :paused="disabled"></WaveCanvas>
         </div>
         <div class="waveCard__buttons">
           <button @click="playWaveBtn">play wave</button>
@@ -40,195 +40,200 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, type Ref, computed } from 'vue';
-import WaveCanvas from '../components/Waves/WaveCanvas.vue';
-import Wave, { waveForms } from '@/models/wave';
-import VerticalSlider from '@/components/Common/VerticalSlider.vue';
-import Selector from '@/components/Common/Selector.vue';
-import AudioModuleCard from '@/components/Waves/AudioModuleCard.vue';
-import ToggleButton from '@/components/Common/ToggleButton.vue';
+  import { onMounted, onUnmounted, ref, type Ref, computed } from 'vue';
+  import WaveCanvas from '../components/Waves/WaveCanvas.vue';
+  import Wave, { waveForms } from '@/models/wave';
+  import VerticalSlider from '@/components/Common/VerticalSlider.vue';
+  import Selector from '@/components/Common/Selector.vue';
+  import AudioModuleCard from '@/components/Waves/AudioModuleCard.vue';
+  import ToggleButton from '@/components/Common/ToggleButton.vue';
 
-const emit = defineEmits(['updateWave', 'deleteWave', "toggleWave"]);
-const props = defineProps({
-  wave: {
-    type: Wave,
-    required: true,
-    default: new Wave(2, 2, 2),
-  },
+  const emit = defineEmits(['updateWave', 'deleteWave', "toggleWave"]);
+  const props = defineProps({
+    wave: {
+      type: Wave,
+      required: true,
+      default: new Wave(2, 2, 2),
+    },
 
-  name: {
-    type: String,
-    default: "wave "
+    name: {
+      type: String,
+      default: "wave "
+    }
+  });
+
+  let audioContext: AudioContext;
+  let gainNode: GainNode;
+  let oscillator: OscillatorNode;
+  let isPlaying: Ref<Boolean> = ref(false);
+  let disabled = ref();
+
+  let disabledStyles = computed(() => {
+    return disabled.value
+      ? "waveCard waveCard--disabled"
+      : "waveCard"
+  })
+
+  function playWaveBtn() {
+    isPlaying.value ? audioContext.suspend() : audioContext.resume();
+    gainNode.gain.setValueAtTime(props.wave.getAmplitude() / 50, audioContext.currentTime);
+    isPlaying.value = !isPlaying.value;
   }
-});
 
-let audioContext: AudioContext;
-let gainNode: GainNode;
-let oscillator: OscillatorNode;
-let isPlaying: Ref<Boolean> = ref(false);
-let disabled = ref();
+  function deleteWave() {
+    oscillator.stop();
+    audioContext.close();
+    emit("deleteWave")
+  }
 
-let applyDisabled = computed(() => {
-  return disabled.value
-    ? "waveCard waveCard--disabled"
-    : "waveCard"
-})
-
-function playWaveBtn() {
-  isPlaying.value ? audioContext.suspend() : audioContext.resume();
-  gainNode.gain.setValueAtTime(props.wave.getAmplitude() / 50, audioContext.currentTime);
-  isPlaying.value = !isPlaying.value;
-}
-
-function deleteWave() {
-  oscillator.stop();
-  audioContext.close();
-  emit("deleteWave")
-}
-
-function updateOscillator() {
-  oscillator.frequency.value = props.wave.getFrequency();
-  gainNode.gain.setValueAtTime(props.wave.getAmplitude() / 50, audioContext.currentTime);
-  oscillator.type = props.wave.getForm();
-}
-
-function onWaveChangeCB() {
-  updateOscillator();
-  emit('updateWave');
-}
-
-function disableWave() {
-  console.log(radioBtn.value)
-}
+  function updateOscillator() {
+    oscillator.frequency.value = props.wave.getFrequency();
+    gainNode.gain.setValueAtTime(props.wave.getAmplitude() / 50, audioContext.currentTime);
+    oscillator.type = props.wave.getForm();
+  }
 
 
-onMounted(() => {
-  audioContext = new AudioContext();
-  audioContext.suspend();
+  function onWaveChangeCB() {
+    updateOscillator();
+    emit('updateWave');
+  }
 
-  gainNode = audioContext.createGain();
-  gainNode.connect(audioContext.destination);
+  function disableWave() {
+    if (disabled.value) {
+      audioContext.suspend()
+    } else {
 
-  oscillator = audioContext.createOscillator();
-  oscillator.frequency.value = props.wave.getFrequency();
-  oscillator.start();
-  oscillator.connect(gainNode);
-});
+    }
+  }
 
-onUnmounted(() => {
-})
+
+  onMounted(() => {
+    audioContext = new AudioContext();
+    audioContext.suspend();
+
+    gainNode = audioContext.createGain();
+    gainNode.connect(audioContext.destination);
+
+    oscillator = audioContext.createOscillator();
+    oscillator.frequency.value = props.wave.getFrequency();
+    oscillator.start();
+    oscillator.connect(gainNode);
+  });
+
+  onUnmounted(() => {
+  })
 </script>
 
 <style scoped lang="scss">
-$card-width: 30rem;
-$card-height: 15rem;
-$card-padding: 2rem;
+  $card-width: 30rem;
+  $card-height: 15rem;
+  $card-padding: 2rem;
 
-.waveCard {
-  // max-width: $card-width;
-  width: 100%;
-  // max-height: $card-height;
-  height: 100%;
-
-  display: grid;
-  grid-template-columns: 1fr 2fr 8fr;
-
-  // box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.5);
-  // border-radius: 20px;
-  // overflow: hidden;
-
-  &--disabled {
-    background-color: red;
-  }
-
-  &__left-slot {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  .waveCard {
+    // max-width: $card-width;
     width: 100%;
     // max-height: $card-height;
     height: 100%;
-    padding: $card-padding 1rem;
-    gap: .5rem;
-  }
 
-  &__controls {
-    max-height: 100%;
-    display: flex;
-    margin: 0 2rem;
-  }
+    display: grid;
+    grid-template-columns: 1fr 2fr 8fr;
 
-  &__control {
-    &__slider {
-      height: 90%;
+    // box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.5);
+    // border-radius: 20px;
+    // overflow: hidden;
+
+    &--disabled {
+      background-color: red;
+    }
+
+    &__left-slot {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
       width: 100%;
+      // max-height: $card-height;
+      height: 100%;
+      padding: $card-padding 1rem;
+      gap: .5rem;
     }
 
-    &__valueBox {
-      display: block;
-      text-align: center;
+    &__controls {
+      max-height: 100%;
+      display: flex;
+      margin: 0 2rem;
+    }
+
+    &__control {
+      &__slider {
+        height: 90%;
+        width: 100%;
+      }
+
+      &__valueBox {
+        display: block;
+        text-align: center;
+      }
+    }
+
+    &__center-slot {
+      display: flex;
+      justify-content: ceter;
+      align-items: center;
+      flex-direction: column;
+      padding: $card-padding 2rem $card-padding 1rem;
+      gap: 1rem;
+    }
+
+    &__wave-slot {
+      width: 100%;
+      height: 100%;
+    }
+
+    &__actions-slot {
+      padding: 1rem 0;
+      background-color: black;
+      // display: flex;
+      // justify-content: center;
+      // align-items: start;
+    }
+
+    &__buttons {
+      display: flex;
+      gap: 1rem;
     }
   }
 
-  &__center-slot {
-    display: flex;
-    justify-content: ceter;
-    align-items: center;
-    flex-direction: column;
-    padding: $card-padding 2rem $card-padding 1rem;
-    gap: 1rem;
-  }
-
-  &__wave-slot {
+  canvas {
+    border-radius: 10px;
+    background-color: var(--canvas-bg);
     width: 100%;
     height: 100%;
   }
 
-  &__actions-slot {
-    padding: 1rem 0;
-    background-color: black;
-    // display: flex;
-    // justify-content: center;
-    // align-items: start;
+  #disableWaveInput {
+    display: none;
   }
 
-  &__buttons {
-    display: flex;
-    gap: 1rem;
-  }
-}
-
-canvas {
-  border-radius: 10px;
-  background-color: var(--canvas-bg);
-  width: 100%;
-  height: 100%;
-}
-
-#disableWaveInput {
-  display: none;
-}
-
-label {
-  width: 1.5rem;
-  height: 1.5rem;
-  background-color: red;
-  border-radius: 100px;
-  position: relative;
-
-  &::before {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  label {
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: red;
     border-radius: 100px;
-    content: "";
-    width: 1rem;
-    height: 1rem;
-    background-color: blue;
-    z-index: 99;
+    position: relative;
+
+    &::before {
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 100px;
+      content: "";
+      width: 1rem;
+      height: 1rem;
+      background-color: blue;
+      z-index: 99;
+    }
   }
-}
 </style>
