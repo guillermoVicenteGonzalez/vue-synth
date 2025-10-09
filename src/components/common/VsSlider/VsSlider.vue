0237d175baabf11b27f63df11fa8ffa993cdf741
Guillermo Vicente González <guillermovg@usal.es>
2025-10-09 12:22:27 +0200

    feat(sliders): refactored vsSlider to use custom styles

--- src/components/common/VsSlider/VsSlider.vue
+++ src/components/common/VsSlider/VsSlider.vue
@@ -54,16 +54,63 @@ const sliderClass = computed(() => {
 </script>
 
 <style lang="scss" scoped>
+$slider-width: 1.5rem;
+
 .slider {
 	box-sizing: border-box;
 	width: 100%;
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
+	align-items: center;
 	gap: 0.5rem;
 
 	&__input {
 		box-sizing: inherit;
+
+		// webkit overrides
+		-webkit-appearance: none; /* Override default CSS styles */
+		appearance: none;
+
+		width: 100%; /* Full-width */
+		height: $slider-width; /* Specified height */
+
+		background: $bg-color-black; /* Grey background */
+		outline: none; /* Remove outline */
+
+		overflow: hidden;
+		border-radius: $border-radius-df;
+
+		// opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+		-webkit-transition: 0.2s; /* 0.2 seconds transition on hover */
+		transition: opacity 0.2s;
+
+		&::-moz-range-track,
+		&::-webkit-slider-runnable-track {
+			height: $slider-width;
+			background: $bg-color-black;
+			border-radius: $border-radius-df;
+		}
+
+		&::-webkit-slider-thumb {
+			-webkit-appearance: none; /* Override default look */
+			appearance: none;
+
+			width: $slider-width; /* Set a specific slider handle width */
+			height: $slider-width; /* Slider handle height */
+			background: $primary-color; /* Green background */
+			border-radius: 50%;
+			cursor: pointer; /* Cursor on hover */
+			box-shadow: -407px 0 0 400px $primary-color;
+		}
+
+		&::-moz-range-thumb {
+			width: 25px; /* Set a specific slider handle width */
+			height: 25px; /* Slider handle height */
+			background: $primary-color; /* Green background */
+			cursor: pointer; /* Cursor on hover */
+			box-shadow: -407px 0 0 400px $primary-color;
+		}
 	}
 
 	&__label {
@@ -76,11 +123,23 @@ const sliderClass = computed(() => {
 	&--vertical {
 		flex-direction: column;
 		height: 100%;
+		width: $slider-width;
+
 		.slider__input {
 			max-height: 100%;
 			height: 100%;
 			writing-mode: vertical-lr;
 			direction: rtl;
+
+			&::-moz-range-track,
+			&::-webkit-slider-runnable-track {
+				height: 100%;
+				width: $slider-width;
+			}
+
+			&::-webkit-slider-thumb {
+				box-shadow: 0 407px 0 400px $primary-color;
+			}
 		}
 	}
 }
